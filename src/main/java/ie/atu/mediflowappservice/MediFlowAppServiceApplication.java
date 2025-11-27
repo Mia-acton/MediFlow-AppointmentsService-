599a89f9373f8cf64eb6c0aa31b7ecf78b34398e
Mia Acton <G00428601@atu.ie>
2025-11-27 23:34:46 +0000

    Created new project

---
+++ src/main/java/ie/atu/mediflowappservice/MediFlowAppServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.mediflowappservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediFlowAppServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediFlowAppServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/mediflowappservice/MediFlowAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.mediflowappservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediFlowAppServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
